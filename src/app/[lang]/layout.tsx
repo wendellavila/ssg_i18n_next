905b38026b34ae9f2972744e7b939d6f80530d9d
wendellavila <wendelljcavila@gmail.com>
2024-04-18 14:51:56 -0300

    fix: Moved NextIntlClientProvider to InternationalizedLayout

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,10 +1,12 @@
 import { archivo } from '@/utils/constants';
 import { getTranslationMessages } from '@/utils/functions';
-import { ClientProviderLoader } from '@/components/client';
 export { generateStaticParams, generateMetadata } from '@/utils/functions';
 
 import { Header } from '@/components/server';
 import { LanguageSelector } from '@/components/client';
+
+import { timezone } from '@/utils/constants';
+import { NextIntlClientProvider } from 'next-intl';
 
 export default async function InternationalizedLayout({
   children, params: {lang}
@@ -16,13 +18,19 @@ export default async function InternationalizedLayout({
   return (
     <html lang={lang}>
       <body className={`${archivo.className}`}>
-        <ClientProviderLoader lang={lang} messages={messages}>
+        <NextIntlClientProvider
+          locale={lang}
+          messages={messages}
+          // timezone and now must be set when using output: export
+          timeZone={timezone}
+          now={new Date()}
+        >
           <div className="min-h-screen flex flex-col">
             <Header/>
             {children}
             <LanguageSelector/>
           </div>
-        </ClientProviderLoader>
+        </NextIntlClientProvider>
       </body>
     </html>
   );
--- src/components/client.tsx
+++ src/components/client.tsx
@@ -1,5 +1,4 @@
 export { default as ClientI18nRedirect } from '@/components/client/ClientI18nRedirect';
-export { default as ClientProviderLoader } from '@/components/client/ClientProviderLoader';
 export { default as Content } from '@/components/client/Content';
 export { default as Iconify } from '@/components/client/Iconify';
 export { default as LanguageSelector } from '@/components/client/LanguageSelector';
