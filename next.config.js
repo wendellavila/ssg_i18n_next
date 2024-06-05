8dde54306a13d62d2b57e7d2842215ca539ea1ae
wendellavila <wendelljcavila@gmail.com>
2024-06-05 08:08:31 -0300

    chore: Fixed basePath in next.config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const isProduction = process.env.NODE_ENV === 'production';
-const basePath = isProduction ? process.env.NEXT_PUBLIC_BASE_PATH : undefined;
+const basePath = isProduction ? '/projects/ssg_i18n_next' : undefined;
 
 const nextConfig = {
     basePath: basePath,
